31a1461d3cfacb07d15724ae9df54c47bef3fd99
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2022-08-20 22:10:12 +0200

    Google analytics basis ?

--- src/main.js
+++ src/main.js
@@ -5,6 +5,29 @@ import router from "./router";
 import { createPinia } from 'pinia'; 
 import dayjs from 'dayjs';
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDq-2dRU8uR_YJQqDCFUxatpnHXb0WTSsY",
+    authDomain: "portfolio-website-v2-yep.firebaseapp.com",
+    projectId: "portfolio-website-v2-yep",
+    storageBucket: "portfolio-website-v2-yep.appspot.com",
+    messagingSenderId: "594557971601",
+    appId: "1:594557971601:web:bc10f045623088bb922330",
+    measurementId: "G-X40VTF7BQQ"
+  };
+
+// Initialize Firebase
+const app_firebase = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app_firebase);
+
 const app = createApp(App);
 const pinia = createPinia();
 
