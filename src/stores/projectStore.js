f0b80adb4220b42481a562e1cda2ce64396b3726
Eliaz Le Riche <47760515+Eliaz-LR@users.noreply.github.com>
2023-05-01 19:14:18 +0200

    Update of the technologies and everything else

--- src/stores/projectStore.js
+++ src/stores/projectStore.js
@@ -1,58 +1,71 @@
 import { defineStore } from "pinia";
 
-export const useProjectStore = defineStore("projectStore",{
-    state: () => ({
-        projects: [
-            {
-                id: 1,
-                name: "This portfolio website",
-                image : new URL("../assets/proj_pic/Website2.png", import.meta.url) .href,
-                description : "This is my second portfolio website. I wanted to make it more user friendly.",
-                technologies: ["Vue.js", "JavaScript", "HTML", "CSS"],
-                link: "https://eliaz-lr.dev/",
-                featured: false,
-            },
-            {
-                id: 2,
-                name: "3D Minesweeper",
-                image : new URL("../assets/proj_pic/3dMinesweeper.png", import.meta.url) .href,
-                description : "A minesweeper game in 3D and first person view with an online leaderboard.",
-                technologies: ["Unity", "C#", "Firebase"],
-                link: "https://play.unity.com/mg/other/webgl-build-482",
-                featured: true,
-            },
-            {
-                id: 3,
-                name: "My old portfolio website",
-                image : new URL("../assets/proj_pic/OldWebsite.png", import.meta.url) .href,
-                description : "This was my first project with Javascript. It emulates the feel of a command line interface.",
-                technologies: ["Unity", "C#", "Firebase"],
-                link: "https://old.eliaz-lr.dev/",
-                featured: true,
-            },
-            {
-                id: 4,
-                name: "More projects available on my GitHub...",
-                image : new URL("../assets/proj_pic/GitHub.png", import.meta.url) .href,
-                description : "I have more projects on my GitHub. Check them out!",
-                technologies: [],
-                link: "https://github.com/Eliaz-LR",
-                featured: false,
-            },
-            {
-                id: 5,
-                name: "Golf Game",
-                image : new URL("../assets/proj_pic/GolfGame.png", import.meta.url) .href,
-                description : "A Golf game made in Unity.",
-                technologies: ["Unity", "C#", "Firebase"],
-                link: "https://github.com/Eliaz-LR/GolfGame",
-                featured: true,
-            }
+export const useProjectStore = defineStore("projectStore", {
+  state: () => ({
+    projects: [
+      {
+        name: "Lyrical Bomb",
+        image: new URL("../assets/proj_pic/LyricalBomb.png", import.meta.url),
+        description:
+          "A lobby based multiplayer game where players have to guess songs that contain a given word in their lyrics. It utilizes Socket.io and the Genius API.",
+        technologies: [
+          "React",
+          "JavaScript",
+          "TypeScript",
+          "HTML",
+          "CSS",
+          "Firebase",
+          "Node.js",
+          "Express.js",
+          "Fly.io",
         ],
-    }),
-    getters: {
-        getProjects: (state) => state.projects,
-        getFeaturedProjects: (state) => state.projects.filter(project => project.featured),
-        getOtherProjects: (state) => state.projects.filter(project => !project.featured),
-    },
+        link: "https://lyrical-bomb.eliaz-lr.dev/",
+        featured: true,
+      },
+      {
+        name: "This portfolio website",
+        image: new URL("../assets/proj_pic/Website2.png", import.meta.url).href,
+        description:
+          "This is my second portfolio website. I wanted to make it more user friendly.",
+        technologies: ["Vue.js", "JavaScript", "HTML", "CSS"],
+        link: "https://eliaz-lr.dev/",
+        featured: true,
+      },
+      {
+        name: "3D Minesweeper",
+        image: new URL("../assets/proj_pic/3dMinesweeper.png", import.meta.url)
+          .href,
+        description:
+          "A minesweeper game in 3D and first person view with an online leaderboard.",
+        technologies: ["Unity", "C#", "Firebase"],
+        link: "https://play.unity.com/mg/other/webgl-build-482",
+        featured: true,
+      },
+      {
+        name: "My old portfolio website",
+        image: new URL("../assets/proj_pic/OldWebsite.png", import.meta.url)
+          .href,
+        description:
+          "This was my first project with Javascript. It emulates the feel of a command line interface.",
+        technologies: ["Unity", "C#", "Firebase"],
+        link: "https://old.eliaz-lr.dev/",
+        featured: true,
+      },
+      {
+        name: "Golf Game",
+        image: new URL("../assets/proj_pic/GolfGame.png", import.meta.url).href,
+        description: "A Golf game made in Unity.",
+        technologies: ["Unity", "C#", "Firebase"],
+        link: "https://github.com/Eliaz-LR/GolfGame",
+        featured: true,
+      },
+    ],
+  }),
+  getters: {
+    getProjects: (state) => state.projects,
+    getFeaturedProjects: (state) =>
+      state.projects.filter((project) => project.featured),
+    getOtherProjects: (state) =>
+      state.projects.filter((project) => !project.featured),
+  },
 });
--- src/stores/technologiesStore.js
+++ src/stores/technologiesStore.js
@@ -1,91 +1,135 @@
 import { defineStore } from "pinia";
 
-export const useTechnologiesStore = defineStore("technologiesStore",{
-    state: () => ({
-        technologies: [
-            {
-                name: "Python",
-                image : new URL("../assets/tech_logo/python.svg", import.meta.url) .href,
-                level: 3,
-                category: "Programming Language",
-                comments: "Python is my go-to language when making projects in many areas such as AI, Data Science and automatisation.",
-            },
-            {
-                name: "Vue.js",
-                image : new URL("../assets/tech_logo/vue.svg", import.meta.url) .href,
-                level: 3,
-                category: "Framework",
-                comments: "Vue.js is my go-to framework when making web applications.",
-            },
-            {
-                name: "C#",
-                image : new URL("../assets/tech_logo/csharp.svg", import.meta.url) .href,
-                level: 2,
-                category: "Programming Language",
-                comments: "I've used C# with Unity to make games and with .NET to make desktop applications.",
-            },
-            {
-                name: "JavaScript",
-                image : new URL("../assets/tech_logo/javascript.svg", import.meta.url) .href,
-                level: 2,
-                category: "Programming Language",
-                comments: "I've used JavaScript to make my websites and web applications.",
-            },
-            {
-                name: "Git",
-                image : new URL("../assets/tech_logo/git.svg", import.meta.url) .href,
-                level: 4,
-                category: "Other",
-                comments: "Git is my go-to version control system. I've used it for many projects and I'm very familiar with it.",
-            },
-            {
-                name: "C++",
-                image : new URL("../assets/tech_logo/cpp.svg", import.meta.url) .href,
-                level: 1,
-                category: "Programming Language",
-                comments: "I've used C and C++ in school to learn the basics of programming.",
-            },
-            {
-                name: "Java",
-                image : new URL("../assets/tech_logo/java.svg", import.meta.url) .href,
-                level: 1,
-                category: "Programming Language",
-                comments: "I've used Java mainly in school.",
-            },
-            {
-                name: "Ruby",
-                image : new URL("../assets/tech_logo/ruby.svg", import.meta.url) .href,
-                level: 1,
-                category: "Programming Language",
-                comments: "I've used Ruby mainly in school.",
-            },
-            {
-                name: "Tailwind CSS",
-                image : new URL("../assets/tech_logo/tailwindcss.svg", import.meta.url) .href,
-                level: 3,
-                category: "Framework",
-                comments: "I've used Tailwind CSS to make my websites and web applications.",
-            },
-            {
-                name: "Firebase",
-                image : new URL("../assets/tech_logo/firebase.svg", import.meta.url) .href,
-                level: 3,
-                category: "Other",
-                comments: "I regularly use Firebase to host my websites and databases.",
-            },
-            {
-                name: "Vite",
-                image : new URL("../assets/tech_logo/vite.svg", import.meta.url) .href,
-                level: 3,
-                category: "Other",
-                comments: "I've used Vite to make my websites and web applications. It provides a dev server and a build system.",
-            },
-        ],
-    }),
-    getters: {
-        getTechnologies: (state) => state.technologies,
-        getProgrammingLanguages: (state) => state.technologies.filter(technology => technology.category === "Programming Language").sort((a,b) => b.level - a.level),
-        getFrameworks: (state) => state.technologies.filter(technology => technology.category === "Framework").sort((a,b) => b.level - a.level),
-        getOther: (state) => state.technologies.filter(technology => technology.category === "Other").sort((a,b) => b.level - a.level),
-    },
+export const useTechnologiesStore = defineStore("technologiesStore", {
+  state: () => ({
+    technologies: [
+      {
+        name: "Python",
+        image: new URL("../assets/tech_logo/python.svg", import.meta.url).href,
+        level: 3,
+        category: "Programming Language",
+        comments:
+          "Python is my go-to language when making projects in many areas such as AI, Data Science and automatisation.",
+      },
+      {
+        name: "Vue.js",
+        image: new URL("../assets/tech_logo/vue.svg", import.meta.url).href,
+        level: 2,
+        category: "Framework",
+        comments:
+          "Vue.js is my first front end framework. I've used it on two projects, including this website.",
+      },
+      {
+        name: "React",
+        image: new URL("../assets/tech_logo/react.svg", import.meta.url).href,
+        level: 2,
+        category: "Framework",
+        comments:
+          "React is the latest front end framework I've started learning. I've used it on Lyrical Bomb.",
+      },
+      {
+        name: "C#",
+        image: new URL("../assets/tech_logo/csharp.svg", import.meta.url).href,
+        level: 2,
+        category: "Programming Language",
+        comments:
+          "I've used C# with Unity to make games and with .NET to make desktop applications.",
+      },
+      {
+        name: "JavaScript",
+        image: new URL("../assets/tech_logo/javascript.svg", import.meta.url)
+          .href,
+        level: 2,
+        category: "Programming Language",
+        comments:
+          "I've used JavaScript to make my websites and web applications.",
+      },
+      {
+        name: "TypeScript",
+        image: new URL("../assets/tech_logo/typescript.svg", import.meta.url)
+          .href,
+        level: 2,
+        category: "Programming Language",
+        comments:
+          "I've recently started using TypeScript instead of JavaScript when possible. I appreciate the added type safety and the better IDE support. I've used it on Lyrical Bomb.",
+      },
+      {
+        name: "Git",
+        image: new URL("../assets/tech_logo/git.svg", import.meta.url).href,
+        level: 3,
+        category: "Other",
+        comments:
+          "Git is my go-to version control system. I've used it for many projects and I'm very familiar with it. While I tend to use GUIs for most of my Git needs, I'm also familiar with the command line.",
+      },
+      {
+        name: "C++",
+        image: new URL("../assets/tech_logo/cpp.svg", import.meta.url).href,
+        level: 1,
+        category: "Programming Language",
+        comments:
+          "I've used C and C++ in school to learn the basics of programming.",
+      },
+      {
+        name: "Java",
+        image: new URL("../assets/tech_logo/java.svg", import.meta.url).href,
+        level: 1,
+        category: "Programming Language",
+        comments: "I've used Java mainly in school.",
+      },
+      {
+        name: "Ruby",
+        image: new URL("../assets/tech_logo/ruby.svg", import.meta.url).href,
+        level: 1,
+        category: "Programming Language",
+        comments: "I've used Ruby mainly in school.",
+      },
+      {
+        name: "Tailwind CSS",
+        image: new URL("../assets/tech_logo/tailwindcss.svg", import.meta.url)
+          .href,
+        level: 4,
+        category: "Framework",
+        comments:
+          "I've used Tailwind CSS to make my websites and web applications.",
+      },
+      {
+        name: "Firebase",
+        image: new URL("../assets/tech_logo/firebase.svg", import.meta.url)
+          .href,
+        level: 3,
+        category: "Other",
+        comments: "I regularly use Firebase to host my websites and databases.",
+      },
+      {
+        name: "Fly.io",
+        image: new URL("../assets/tech_logo/fly.svg", import.meta.url).href,
+        level: 2,
+        category: "Other",
+        comments: "I used Fly.io to host my backend for Lyrical Bomb.",
+      },
+      {
+        name: "Vite",
+        image: new URL("../assets/tech_logo/vite.svg", import.meta.url).href,
+        level: 3,
+        category: "Other",
+        comments:
+          "I've used Vite to make my websites and web applications. It provides a dev server and a build system.",
+      },
+    ],
+  }),
+  getters: {
+    getTechnologies: (state) => state.technologies,
+    getProgrammingLanguages: (state) =>
+      state.technologies
+        .filter((technology) => technology.category === "Programming Language")
+        .sort((a, b) => b.level - a.level),
+    getFrameworks: (state) =>
+      state.technologies
+        .filter((technology) => technology.category === "Framework")
+        .sort((a, b) => b.level - a.level),
+    getOther: (state) =>
+      state.technologies
+        .filter((technology) => technology.category === "Other")
+        .sort((a, b) => b.level - a.level),
+  },
 });
